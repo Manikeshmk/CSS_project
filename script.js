94c4508fc8823937d8b1a4f34c28f708de3cad9c
Manikeshmk <23bds032@iiitdwd.ac.in>
2026-05-08 11:44:28 +0530

    add glassmorphism

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-// Interactive rotation based on mouse movement
-document.addEventListener('mousemove', (e) => {
-    const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    const cube = document.querySelector('.cube');
-    cube.style.animation = 'none';
-    cube.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('mousemove', (e) => {
+        let x = (window.innerWidth / 2 - e.pageX) / 30;
+        let y = (window.innerHeight / 2 - e.pageY) / 30;
+        card.style.transform = `rotateY(${x}deg) rotateX(${y}deg) translateZ(50px)`;
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = `rotateY(0deg) rotateX(0deg) translateZ(0px)`;
+    });
 });
